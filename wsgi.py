e9663a29b1d7eb3d71741961eb94e023d2ab5fe7
Samuel Littley <samuel.littley@toastwaffle.com>
2013-11-12 12:56:04 +0000

    Fix wsgi paths

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import site, os, sys
-site.addsitedir(os.path.realpath(__file__).replace('kebleball','lib/python2.7/site-packages'))
-sys.path.append(os.path.realpath(__file__))
+site.addsitedir(os.path.realpath(__file__).replace('/kebleball/','/lib/python2.7/site-packages/').replace('/wsgi.py',''))
+sys.path.append(os.path.realpath(__file__).replace('/wsgi.py',''))
 
 from kebleball import app as application
 
