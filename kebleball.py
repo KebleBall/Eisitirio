84b9aea89153d92279350bb8b651bc8a6b4da14b
Samuel Littley <samuel.littley@toastwaffle.com>
2013-12-09 16:14:11 +0000

    Change config

--- kebleball.py
+++ kebleball.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from os import getenv
+
 app = Flask(__name__)
+app.config.from_object('config.default')
+
+if getenv('KEBLE_BALL_ENV', 'DEVELOPMENT') == 'PRODUCTION':
+    app.config.from_object('config.production')
+else:
+    app.config.from_object('config.development')
+
+@app.route('/hello')
+def hello_world():
+    return ('Hello World! I am the Keble Ball Ticketing System, '
+        'running in the {0} environment').format(app.config['ENVIRONMENT'])
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    raise NotImplementedError
 
 if __name__ == '__main__':
     app.run()
