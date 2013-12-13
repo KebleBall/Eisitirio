7d038b8b08c883dbf9fb406b6521968e20ce02ac
Samuel Littley <samuel.littley@toastwaffle.com>
2013-12-13 22:08:07 +0000

    Add cronjob worker placeholder with todos

---
+++ cron_worker.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+
+# [idea] - Store last timestamp somewhere, use to determine which tasks to do
+
+# [todo] - Send announcement emails
+# [todo] - Process waiting list
+# [todo] - Remove expired tickets
+# [todo] - Update statistics
+# [todo] - Send reminder emails for expiring tickets
+# [todo] - Remove expiring secret keys
