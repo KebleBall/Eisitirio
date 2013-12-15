743c1dc4800fe4041a331c59403cae6cb9a92711
Samuel Littley <samuel.littley@toastwaffle.com>
2013-12-15 17:36:40 +0000

    Debug on production

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
 import site, os, sys
 site.addsitedir(os.path.realpath(__file__).replace('/kebleball/','/lib/python2.7/site-packages/').replace('/wsgi.py',''))
 sys.path.append(os.path.realpath(__file__).replace('/wsgi.py',''))
+from werkzeug.debug import DebuggedApplication
 
-from kebleball import app as _application
+from kebleball import app
 
 def application(req_environ, start_response):
     os.environ['KEBLE_BALL_ENV'] = req_environ['KEBLE_BALL_ENV']
+    _application = DebuggedApplication(app, True)
     return _application(req_environ, start_response)
