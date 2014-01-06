9ee083cb043e1ec13a8b9b6a60ce3804c80091bb
Samuel Littley <samuel.littley@toastwaffle.com>
2014-01-06 21:47:50 +0000

    Debug production env

--- kebleball/app.py
+++ kebleball/app.py
@@ -4,8 +4,4 @@ from os import environ
 app = Flask(__name__)
 
 app.config.from_pyfile('config/default.py')
-
-if 'KEBLE_BALL_ENV' in environ and environ['KEBLE_BALL_ENV'] == 'PRODUCTION':
-    app.config.from_pyfile('config/production.py')
-else:
-    app.config.from_pyfile('config/development.py')
+app.config.from_pyfile('config/development.py')
