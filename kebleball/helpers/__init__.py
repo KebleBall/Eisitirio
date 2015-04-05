610c1ece2c38bfe23a92be009481a446e48363fe
Samuel Littley <samuel.littley@toastwaffle.com>
2015-04-05 14:12:39 +0100

    Lint helpers.__init__
    Change-Id: I8df5f4357e02e606961aa8f81afbf685aeb50086

--- kebleball/helpers/__init__.py
+++ kebleball/helpers/__init__.py
@@ -1,9 +1,23 @@
 # coding: utf-8
-from datetime import datetime
+"""Module containing helper functions and classes."""
+
 import random
 import string
 
 def generate_key(length, choices=None):
+    """Generate a randomised key of a given length.
+
+    Random keys are used for verifying the authenticity of email confirmations,
+    password resets and resale actions.
+
+    Args:
+        length: (int) Length (in characters) of the returned string
+        choices: (list or None) List of characters to use in the key. If not
+            given, defaults to alphanumerics
+
+    Returns:
+        (str) a randomly generated string
+    """
     if choices is None:
         choices = (
             string.ascii_lowercase +
