22f4235ee6a665670e551c267fc5b9bcbf00e423
Samuel Littley <samuel.littley@toastwaffle.com>
2015-04-11 00:23:14 +0100

    Start work on automatic review submission script
    Change-Id: I51dbe53f0f4fb607af79ad86d8461c570072a7e4

---
+++ review.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python2
+
+from __future__ import unicode_literals
+from __future__ import with_statement
+
+import os
+import tempfile
+import json
+import subprocess
+import sys
+
+CMD_GET_BRANCH = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
+
+
+def cmd_get_diffbase(branch):
+    return ['git', 'merge-base', 'master', branch]
+
+
+def cmd_get_sha(ref):
+    return ['git', 'rev-parse', ref]
+
+
+def cmd_get_diff(base, target):
+    return ['git', 'diff', base, target]
+
+
+def cmd_change_branch(branch, new=False):
+    cmd = ['git', 'checkout']
+
+    if new:
+        cmd.append('-b')
+
+    cmd.append(branch)
+
+    return cmd
+
+
+def cmd_apply_patch(patchfile):
+    return ['git', 'apply', patchfile]
+
+
+def main():
+    try:
+        with open(os.path.realpath(__file__)[:-3] + '.json') as fh:
+            reviews = json.load(fh)
+    except IOError as _:
+        reviews = {}
+
+    branch = subprocess.check_output(CMD_GET_BRANCH).strip()
+
+    if branch == 'HEAD':
+        sys.stderr.write('Cannot review a non-branch\n')
+        sis.exit(1)
+
+    if branch in reviews:
+        diffbase = reviews[branch]
+    else:
+        diffbase = subprocess.check_output(cmd_get_diffbase(branch)).strip()
+
+    temp = tempfile.NamedTemporaryFile(delete=False)
+
+    temp.write(subprocess.check_output(cmd_get_diff(diffbase, branch)))
+
+    temp.close()
+
+    review_branch = 'review_' + branch
+
+    if branch in reviews:
+        subprocess.call(cmd_change_branch(review_branch))
+    else:
+        subprocess.call(cmd_change_branch(diffbase))
+        subprocess.call(cmd_change_branch(review_branch, True))
+
+    subprocess.call(cmd_apply_patch(temp.name))
+
+    os.unlink(temp.name)
+
+    reviews[branch] = subprocess.check_output(cmd_get_sha(branch)).strip()
+
+    with open(os.path.realpath(__file__)[:-3] + '.json', 'w+') as fh:
+        json.dump(reviews, fh)
+
+    subprocess.call(cmd_change_branch(branch))
+
+if __name__ == '__main__':
+    main()
