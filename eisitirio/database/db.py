379ee315bbe5a17e11901ea60c270a6ebf778249
Samuel Littley <samuel.littley@toastwaffle.com>
2016-03-13 16:39:16 +0000

    Make alembic/migrate notice changes to enums
    Change-Id: Iefcd38b21853d56a7eacc1c05b8adf5e5e9137b4

--- eisitirio/database/db.py
+++ eisitirio/database/db.py
@@ -1,13 +1,15 @@
 # coding: utf-8
 """Creates the SQLAlchemy connection object"""
 
-from flask.ext import sqlalchemy
+from flask.ext import sqlalchemy as flask_sqlalchemy
+from sqlalchemy.dialects import mysql
 from sqlalchemy.ext import declarative
+import sqlalchemy
 
 from eisitirio import app
 from eisitirio.database import custom_model
 
-DB = sqlalchemy.SQLAlchemy(app.APP)
+DB = flask_sqlalchemy.SQLAlchemy(app.APP)
 
 # Awkward hack to use a custom model class. Flask-SQLAlchemy 3.0 (in beta as of
 # 2016-01-11) has a model_class parameter to the above constructor, which should
@@ -18,4 +20,15 @@ DB.Model = declarative.declarative_base(
     metadata=None,
     metaclass=custom_model.CustomModelMeta
 )
-DB.Model.query = sqlalchemy._QueryProperty(DB) # pylint: disable=protected-access
+DB.Model.query = flask_sqlalchemy._QueryProperty(DB) # pylint: disable=protected-access
+
+@sqlalchemy.event.listens_for(sqlalchemy.Table, "column_reflect")
+def column_reflect(_, unused, column_info):
+    if isinstance(column_info['type'], mysql.TINYINT):
+        column_info['type'] = sqlalchemy.Boolean()
+
+    if isinstance(column_info['type'], mysql.MEDIUMTEXT):
+        column_info['type'] = sqlalchemy.UnicodeText(length=65536)
+
+    if isinstance(column_info['type'], mysql.TEXT):
+        column_info['type'] = sqlalchemy.UnicodeText(length=256)
