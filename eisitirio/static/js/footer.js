4c6b26021d97857a3dbac3c5eef8d4e30d9081b6
tzakian <timothy.zakian@cs.ox.ac.uk>
2016-10-23 15:14:33 +0100

    Major: Upgrade foundation version

--- eisitirio/static/js/footer.js
+++ eisitirio/static/js/footer.js
@@ -1,7 +1,11 @@
-jQuery('#flashes li').on('click', function(event) {
-    jQuery(event.target).fadeOut(250);
-});
+$(document).ready(function() {
+    $(document).foundation();
 
-jQuery('.message-box.hideable').on('click', function(event) {
-    jQuery(event.target).fadeOut(250);
+    jQuery('#flashes li').on('click', function(event) {
+        jQuery(event.target).fadeOut(250);
+    });
+
+    jQuery('.message-box.hideable').on('click', function(event) {
+        jQuery(event.target).fadeOut(250);
+    });
 });
