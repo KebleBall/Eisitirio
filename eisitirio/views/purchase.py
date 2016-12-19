73a82e40b18862cd82abacef6c4d1309a680d13a
tzakian <timothy.zakian@cs.ox.ac.uk>
2016-12-19 18:45:42 +0000

    work this time?

--- eisitirio/views/purchase.py
+++ eisitirio/views/purchase.py
@@ -567,12 +567,10 @@ def pay_admin_fee(admin_fee_id):
                           flask.url_for('dashboard.dashboard_home'))
 
 @PURCHASE.route('/purchase/test-transaction', methods=['GET', 'POST'])
-@login.login_required
 def test_transaction():
     return flask.render_template('purchase/test_transaction_page.html')
 
-@PURCHASE.route('/purchase/payment-processed', methods=['POST'])
-@login.login_required
+@PURCHASE.route('/purchase/payment-processed', methods=['GET','POST'])
 def payment_processed():
     print request.form
     return "Looks good"
