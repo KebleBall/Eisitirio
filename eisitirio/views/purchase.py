f03d42fd5140391c7e23d78434f90b9e4f016a4e
tzakian <timothy.zakian@cs.ox.ac.uk>
2016-12-22 14:08:52 +0000

    add change id

--- eisitirio/views/purchase.py
+++ eisitirio/views/purchase.py
@@ -555,9 +555,9 @@ def payment_processed():
     if flask.request.method == 'POST':
         print flask.request.form
         return "Looks good: POST request. FORM: {0}\n HEADERS: {1}\n ARGS: {2}".format(request.form, request.headers, request.args)
-        
+
     else:
-        return "Looks good: GET request"
+        return "Looks good: GET request. CHANGE ID: 1"
 
 
 ## This is for eWay
